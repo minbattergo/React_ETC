eba735fc22cc39c01dc6c582f725b463b1064e5b
ssun <pros5018@nate.com>
2019-07-13 12:32:31 +0900

    all commit

---
+++ 리액트강좌.js
@@ -0,0 +1,21 @@
+교육사이트
+http://eastflag.co.kr/
+
+데모사이트
+http://scoreboard.eastflag.co.kr/
+
+과제 및 질문 게시판 
+http://cafe.naver.com/ng2spring
+
+ - node 의 npm모듈이 필요함
+ - 개발에 필요한 툴
+ git        - 형상관리
+ confluence - 산출물 공동개발 (아틀란 시안) : 위키 사이트
+ jira       - 프로젝 계획수립 수정
+
+ - 비쥬얼 스튜디어 코드 (무료)
+ - 웹스톰  (https://www.jetbrains.com/ -> Tools -> WebStrom) 
+   http://edu.javabrain.kr:8750
+
+ - React Elements => DOM과 연결된 javascript 객체를 다룬다.
+ ... : 나머지 연산자(java script), spread 연산자
