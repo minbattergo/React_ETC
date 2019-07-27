b38895035f2997ad14deee2b6e0304fca3be3493
ssun <pros5018@nate.com>
2019-07-27 17:29:18 +0900

    react 3week

---
+++ 리액트강좌_3일차.js
@@ -0,0 +1,10 @@
+//======================================================
+자바스크립트 비동기 패턴
+callback => promise => AysncaWait
+
+google
+ => MDN 자바스크리틈
+ 
+ react-redux =>
+   - subscribe : 부모 -> 자식
+   - publish   : 자식 -> 부모 (callback function으로 했었던)
